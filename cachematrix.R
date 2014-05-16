34c6c1bb618770c553eb18fbd0607c9a53238572
topcat39 <trcleary@gmail.com>
2014-05-16 10:02:44 -0400

    Completed
    To test:
    mx <- makeCacheMatrix()
    mx$set(matrix(runif(16), 4, 4))
    cacheSolve(mx)
    x <- mx$get()
    y <- cacheSolve(mx)
    trunc(x %*% y)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caches an Inverted Matrix for future use
+## An initial matrix must be created first, using
+##    makeCacheMatrix
 
-## Write a short comment describing this function
+
+## makeCacheMatrix - creates a Matrix that acts like a class
+##    sets/gets the value of the matrix
+##    sets/gets the value of the inverted matrix
+##
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      inv <- NULL
+      set <- function(y) {
+            x <<- y
+            inv <<- NULL
+      }
+      get <- function() x
+      setinv <- function(solve) inv <<- solve
+      getinv <- function() inv
+      list(set = set, get = get,
+           setinv = setinv,
+           getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve solves a matrix (return the inverse)
+## if matrix previously solved, simply return the cached matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      inv <- x$getinv()
+      if(!is.null(inv)) {
+            message("getting cached data")
+            return(inv)
+      }
+      data <- x$get()
+      inv <- solve(data, ...)
+      x$setinv(inv)
+      inv
 }
